a0ea65f8695507cd5b70a3e996b483c9ecaddd57
YLuUxhdH <kykucak@gmail.com>
2021-06-27 17:28:38 +0300

    Add is_public, public_link to the Note model

--- notes_manager/notes/models.py
+++ notes_manager/notes/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    is_favorite = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False)
+    public_link = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+
+    def __str__(self):
+        return self.name
